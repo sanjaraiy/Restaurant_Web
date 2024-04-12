7fa51d398cd268c7f122b053907c3247485334e4
sanjaraiy <deepakrai78788@gmail.com>
2024-04-13 01:20:31 +0530

    Done Header Part

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+
+function App() {
+  
+return (
+    <div className="w-screen h-auto flex-col">
+         <Header></Header>
+    </div>
+  )
+}
+
+export default App
